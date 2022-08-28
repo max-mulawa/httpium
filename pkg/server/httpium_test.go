1a215d88ea62ddbb89f0240cb18142b615c00d82
Maksymilian Mulawa <max.mulawa@gmail.com>
2022-08-28 15:58:02 +0200

    404 test

--- pkg/server/httpium_test.go
+++ pkg/server/httpium_test.go
@@ -3,6 +3,7 @@ package server_test
 import (
 	"context"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"os"
@@ -20,6 +21,7 @@ import (
 var (
 	tmpContentDir string
 	servicePort   uint = 8090
+	client             = http.Client{}
 )
 
 func TestMain(m *testing.M) {
@@ -56,16 +58,30 @@ func TestMain(m *testing.M) {
 	}
 }
 
-func Test(t *testing.T) {
-	filePath := path.Join(tmpContentDir, "index.html")
-	err := os.WriteFile(filePath, []byte("<html>ok!</html>"), 0o600)
-	require.NoError(t, err)
+func TestStaticContent(t *testing.T) {
+	t.Run("existing file serverd", func(t *testing.T) {
+		filePath := path.Join(tmpContentDir, "index.html")
+		fileContent := "<html>ok!</html>"
+		err := os.WriteFile(filePath, []byte(fileContent), 0o600)
+		require.NoError(t, err)
 
-	client := http.Client{}
-	resp, err := client.Get(fmt.Sprintf("http://localhost:%d/index.html", servicePort))
-	require.NoError(t, err)
+		resp, err := client.Get(fmt.Sprintf("http://localhost:%d/index.html", servicePort))
+		require.NoError(t, err)
 
-	defer resp.Body.Close()
+		defer resp.Body.Close()
+		require.Equal(t, http.StatusOK, resp.StatusCode)
+		content, err := io.ReadAll(resp.Body)
+		require.NoError(t, err)
 
-	require.Equal(t, http.StatusOK, resp.StatusCode)
+		require.Equal(t, fileContent, string(content))
+	})
+
+	t.Run("for missing file 404 error code returned", func(t *testing.T) {
+		resp, err := client.Get(fmt.Sprintf("http://localhost:%d/index2.html", servicePort))
+		require.NoError(t, err)
+
+		defer resp.Body.Close()
+
+		require.Equal(t, http.StatusNotFound, resp.StatusCode)
+	})
 }
